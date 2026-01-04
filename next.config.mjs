55f6301c1e10733419f8710a0bd247c369e0aa60
Dipanjan Sadhukhan <dipanjansadhukhan123@gmail.com>
2026-01-04 16:57:29 +0530

    Fix: Add basePath for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   /* config options here */
   output: "export",
   reactCompiler: true,
+  basePath: isProd ? "/camp-link" : "",
+  assetPrefix: isProd ? "/camp-link/" : "",
   images: {
     unoptimized: true,
     domains: ["www.jisuniversity.ac.in"],
